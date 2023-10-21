42a31a233a9b237a9febd0f67accba1da3619bb7
leo <carl-leo-benjamin@hotmail.com>
2023-10-21 08:56:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [company, setCompany] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/companies/belowIntrinsicValue");
+        const data = await response.json();
+        setCompany(data);
+      } catch (error) {
+        console.error("Error fetching company data:", error);
+      }
+    };
+
+    fetchData();
+    const intervalId = setInterval(fetchData, 60000); // Poll every 60 seconds
+
+    return () => clearInterval(intervalId); // Cleanup on unmount
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {company ? (
+        <>
+          <h1>{company.name}</h1>
+          <p>PE: {company.pe}</p>
+          <p>EPS: {company.eps}</p>
+        </>
+      ) : (
+        <p>No companies found below intrinsic value.</p>
+      )}
     </div>
   );
 }
